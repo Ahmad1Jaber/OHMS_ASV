fdffe64e31ab004cf1fcd71bd1691c1bf608d04c
Ahmad1Jaber <ahmadjaber1998@gmail.com>
2023-02-24 09:51:10 +0300

    auth module | V-0.0.0.2

--- authentication/app.py
+++ authentication/app.py
@@ -45,3 +45,44 @@ def register():
         'message': 'User registered successfully.'
     }
     return jsonify(response), 200
+
+@app.route('/login', methods=['POST'])
+def login():
+    # Get the request data
+    data = request.json
+
+    # Extract the user details from the request data
+    email = data.get('email')
+    password = data.get('password')
+
+    # Retrieve the user from the database
+    cursor = cnx.cursor()
+    select_query = "SELECT id, password FROM users WHERE email = %s"
+    select_data = (email,)
+    cursor.execute(select_query, select_data)
+    result = cursor.fetchone()
+
+    if result is None:
+        # User not found
+        response = {
+            'status': 'fail',
+            'message': 'Invalid email or password.'
+        }
+        return jsonify(response), 401
+
+    user_id, hashed_password = result
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8')):
+        # Passwords match, login successful
+        response = {
+            'status': 'success',
+            'message': 'User logged in successfully.',
+            'user_id': user_id
+        }
+        return jsonify(response), 200
+    else:
+        # Passwords don't match, login failed
+        response = {
+            'status': 'fail',
+            'message': 'Invalid email or password.'
+        }
+        return jsonify(response), 401
