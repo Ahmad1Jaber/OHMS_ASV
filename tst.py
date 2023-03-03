16bdba9994bf2c37d162d28355ec96fc3b7793a0
Ahmad Adnan Jaber <31273139+Ahmad1Jaber@users.noreply.github.com>
2023-03-03 08:28:10 +0300

    Update tst.py

--- tst.py
+++ tst.py
@@ -4,6 +4,7 @@ url = "http://localhost:5000/login"
 data = {
     "email": "manager@hilton.com",
     "password": "ahmadjaber"
+    
 }
 response = requests.post(url, json=data)
 print(response.text)
