18b215132f8b5f38ba34597132090c83e0b7173c
Ahmad Adnan Jaber <31273139+Ahmad1Jaber@users.noreply.github.com>
2023-03-04 20:59:09 +0300

    Updating Docker file

--- tst.py
+++ tst.py
@@ -1,10 +1,11 @@
 import requests
 
-url = "http://localhost:5000/login"
-data = {
-    "email": "manager@hilton.com",
-    "password": "ahmadjaber"
-    
-}
-response = requests.post(url, json=data)
-print(response.text)
+url = "http://34.154.225.231/login"
+
+for i in range(10):
+    data = {
+        "email": f"manager{i}@hilton.com",
+        "password": f"ahmadjaber{i}"
+    }
+    response = requests.post(url, json=data)
+    print(f"Response {i+1}: {response.text}")
